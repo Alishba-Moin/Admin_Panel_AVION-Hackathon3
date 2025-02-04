0845b70fbb86be32ddb5b4cae0ff5dd548c3ac8a
Alishba Moin <159716625+Alishba-Moin@users.noreply.github.com>
2025-02-04 19:32:23 +0500

    Update page.tsx

--- src/app/Admin/Categories/page.tsx
+++ src/app/Admin/Categories/page.tsx
@@ -1,11 +1,14 @@
+import React, { Suspense } from "react";
 import Form from './components/CategoryForm';
 import ListView from './components/CategoryList';
 
 export default function Page() {
   return (
-    <main className="p-5 flex flex-col md:flex-row gap-5">
-      <Form />
-      <ListView />
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="p-5 flex flex-col md:flex-row gap-5">
+        <Form />
+        <ListView />
+      </main>
+    </Suspense>
   );
 }
